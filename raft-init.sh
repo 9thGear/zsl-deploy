68dfac46c49bab4a56cf5fd474428782a8652557
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-02-27 12:24:44 -0600

    Add minimal needed files to setup of a raft node

---
+++ raft-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -u
+set -e
+
+echo "[*] Cleaning up temporary data directories"
+rm -rf qdata
+mkdir -p qdata/logs
+
+echo "[*] Configuring node 1..."
+mkdir -p qdata/dd2/{keystore,geth}
+cp raft/static-nodes.json qdata/dd2
+cp keys/key2 qdata/dd2/keystore
+cp raft/nodekey2 qdata/dd2/geth/nodekey
+geth --datadir qdata/dd2 init genesis.json
+echo "[*] Configured node 2"
