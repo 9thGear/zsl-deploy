d4967d5f12b5bd2509c66b1950b3061f3a896972
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-02-28 12:12:14 -0600

    Fix execution context for start.sh script

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 set -u
 set -e
 
+cd ./node1
+
 echo "[*] Running raft-init.sh..."
-./node1/raft-init.sh
+./raft-init.sh
 ehco "[*] raft-init.sh Finsihed"
 
 echo "[*] Running raft-start.sh"
-./node1/raft-start.sh
+./raft-start.sh
 echo "[*] raft-start.sh Finished"
 
