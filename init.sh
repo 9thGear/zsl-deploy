ce3e4b2cd1e6ae92e2f2c50ae2476391c0a315c4
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-02-28 12:24:54 -0600

    Make precompiled binaries executable when wget-ed in init.sh

--- init.sh
+++ init.sh
@@ -10,17 +10,17 @@ sudo apt-get install -y build-essential unzip libleveldb-dev libdb-dev libsodium
 echo "[*] Installed Build Dependencies"
 
 echo "[*] Copying the pre-compiled binaries to /usr/local/bin..."
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/abigen && sudo mv abigen /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/bootnode && sudo mv bootnode /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/constellation-enclave-keygen && sudo mv constellation-enclave-keygen /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/constellation-node && sudo mv constellation-node /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/evm && sudo mv evm /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/faucet && sudo mv faucet /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/geth && sudo mv geth /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/puppeth && sudo mv puppeth /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/rlpdump && sudo mv rlpdump /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/swarm && sudo mv swarm /usr/local/bin
-wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/wnode && sudo mv wnode /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/abigen && chmod +x abigen &&  sudo mv abigen /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/bootnode && chmod +x bootnode && sudo mv bootnode /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/constellation-enclave-keygen && chmod +x constellation-enclave-keygen  && sudo mv constellation-enclave-keygen /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/constellation-node && chmod +x constellation-node && sudo mv constellation-node /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/evm && chmod +x evm && sudo mv evm /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/faucet && chmod +x faucet && sudo mv faucet /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/geth && chmod +x geth && sudo mv geth /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/puppeth && chmod +x puppeth && sudo mv puppeth /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/rlpdump && chmod +x rlpdump && sudo mv rlpdump /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/swarm && chmod +x swarm && sudo mv swarm /usr/local/bin
+wget https://github.com/9thGear/zsl-deploy/releases/download/binaries-v0.1.6/wnode && chmod +x wnode && sudo mv wnode /usr/local/bin
 echo "[*] Copied the pre-compiled binaries"
 
 echo "[*] Getting the Quorum ZSL Parameters..."
