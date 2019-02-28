d8c61cfe22377e517b473854176e6bd03bad9bcf
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-02-28 12:46:46 -0600

    Fix typo in start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ cd ./node1
 
 echo "[*] Running raft-init.sh..."
 ./raft-init.sh
-echo "[*] raft-init.sh Finsihed"
+echo "[*] raft-init.sh Finished"
 
 echo "[*] Running raft-start.sh"
 ./raft-start.sh
